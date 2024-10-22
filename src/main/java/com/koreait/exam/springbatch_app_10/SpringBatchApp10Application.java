4f9824c111b5aedaf6340db2a61f2beee6ccfe15
yeosang0508 <yeosang0508@gmail.com>
2024-10-22 15:11:39 +0900

    í프로젝트 세팅

---
+++ src/main/java/com/koreait/exam/springbatch_app_10/SpringBatchApp10Application.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.springbatch_app_10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchApp10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApp10Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/koreait/exam/springbatch_app_10/SpringBatchApp10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreait.exam.springbatch_app_10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApp10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
